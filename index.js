6c5a8f2c4c3b47b0479433424bc388b974333733
mantis <morauszki.andras@gmail.com>
2023-10-05 07:00:50 +0200

    chore: dummy books array

--- index.js
+++ index.js
@@ -1,11 +1,55 @@
+const DUMMY_BOOKS = [
+  {
+    author: 'J.R.R. Tolkien',
+    title: 'Lord of the Rings',
+    coverUrl: '',
+    numPages: 0,
+    read: true,
+    favorite: true,
+  },
+  {
+    author: 'J.R.R. Tolkien',
+    title: 'The Silmarillion',
+    coverUrl: '',
+    numPages: 0,
+    read: true,
+    favorite: true,
+  },
+  {
+    author: 'J.R.R. Tolkien',
+    title: 'The Hobbit',
+    coverUrl: '',
+    numPages: 0,
+    read: true,
+    favorite: false,
+  },
+  {
+    author: 'George R.R. Martin',
+    title: 'Game of Thrones',
+    coverUrl: '',
+    numPages: 0,
+    read: true,
+    favorite: false,
+  },
+  {
+    author: 'Neil Gaiman',
+    title: 'Norse Mythology',
+    coverUrl: '',
+    numPages: 0,
+    read: false,
+    favorite: false,
+  },
+];
+
 const library = [];
 
-function Book(author, title, coverUrl, numPages, status) {
+function Book(author, title, coverUrl, numPages, read, favorite) {
   this.author = author;
   this.title = title;
   this.coverUrl = coverUrl;
   this.numPages = numPages;
-  this.status = status;
+  this.read = read;
+  this.favorite = favorite;
 }
 
 function addBookToLibrary(book) {
