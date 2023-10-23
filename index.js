8aa43ebb97df854fe55b010fd8af492a8583a43f
mantis <morauszki.andras@gmail.com>
2023-10-23 23:20:00 +0200

    work on badges

--- index.js
+++ index.js
@@ -199,6 +199,18 @@ const deleteBook = (event) => {
   bookEl.remove();
 };
 
+const handleBadgeClick = (event) => {
+  const bookEl = event.target.closest('.book');
+  const bookId = bookEl.dataset.id;
+  const book = myLibrary.books.find((book) => book.id === bookId);
+  const badgeEl = event.target.closest('.status-badge');
+  const propertyToToggle = badgeEl.dataset.property;
+
+  book.toggleProperty(propertyToToggle);
+  badgeEl.classList.toggle('active');
+  console.log(badgeEl.querySelector('.badge-text'));
+};
+
 // EVENT LISTENERS
 addBookBtn.addEventListener('click', openAddForm);
 cancelBtn.addEventListener('click', closeAddForm);
@@ -261,28 +273,27 @@ Book.prototype.createMarkup = function () {
           <p class="pages">${this.numPages} pages</p>
         </div>
         <div class="status-badges">
-          <p class="status-badge badge--read ${this.read ? 'active' : ''}">
+          <div class="status-badge badge--read ${
+            this.read ? 'active' : ''
+          }" data-property="read">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="status-icon">
               <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
-            ${this.read ? 'READ' : 'UNREAD'}
-          </p>
-          <p class="status-badge badge--reading ${
+          </div>
+          <div class="status-badge badge--reading ${
             this.currentlyReading ? 'active' : ''
-          }">
+          }" data-property="currentlyReading">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="status-icon">
               <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
             </svg>
-            ${this.currentlyReading ? 'READING' : ''}
-          </p>
-          <p class="status-badge badge--favorite ${
+          </div>
+          <div class="status-badge badge--favorite ${
             this.favorite ? 'active' : ''
-          }">
+          }" data-property="favorite">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="status-icon">
               <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
             </svg>
-            ${this.favorite ? 'FAVORITE' : ''}
-          </p>
+          </div>
         </div>
         <div class="action">
           <button class="delete">
@@ -295,6 +306,11 @@ Book.prototype.createMarkup = function () {
       </div>
     </li>
   `;
+};
+
+Book.prototype.toggleProperty = function (property) {
+  console.log(this[property]);
+  this[property] = !this[property];
 };
 
 function Library(books) {
@@ -328,12 +344,14 @@ const myLibrary = new Library(library);
 myLibrary.renderAllBooks();
 
 // BOOK BADGES & BUTTONS
+const badges = document.querySelectorAll('.status-badge');
 const readBadges = document.querySelectorAll('.badge--read');
 const readingBadges = document.querySelectorAll('.badge--reading');
 const favoriteBadges = document.querySelectorAll('.badge--favorite');
 const deleteButtons = document.querySelectorAll('button.delete');
 
 deleteButtons.forEach((btn) => btn.addEventListener('click', deleteBook));
+badges.forEach((badge) => badge.addEventListener('click', handleBadgeClick));
 
 // TODO: Confirmation modal before delete
 // TODO: Toggle read functionality
